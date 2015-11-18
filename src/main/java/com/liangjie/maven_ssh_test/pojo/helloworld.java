f4e5f3b23ce9ffa9cc358acc50089987687f4519
liangjie <781697393@qq.com>
2015-11-18 11:18:35 +0800

    修改了目录，使得包的命名和maven的groupid和artifactId相同

---
+++ src/main/java/com/liangjie/maven_ssh_test/pojo/helloworld.java
@@ -0,0 +1,11 @@
+package com.liangjie.maven_ssh_test.pojo;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("111");
+
+	}
+
+}
