19a27b5766228ac6f609ab629b8b46ee51e1c5c4
liangjie <781697393@qq.com>
2015-11-20 16:54:24 +0800

    整理了项目下的文件，使得src/main/resources下文件分明，不同的框架对应不同的文件夹，防止配置文件结构泛滥

--- src/test/java/com/lj/mvn_ssh/service/TestSpring.java
+++ src/test/java/com/lj/mvn_ssh/service/TestSpring.java
@@ -13,7 +13,7 @@ public class TestSpring {
 	//出现了找不到驱动包的错误，最好查找出是因为dataSource.properties下后面有俩个空格，但是console下输出的空格又不清楚，找了很久
 	@Test
 	    public void test() {  
-	        ApplicationContext ac = new ClassPathXmlApplicationContext(new String[] { "classpath:applicationContext.xml" });  
+	        ApplicationContext ac = new ClassPathXmlApplicationContext(new String[] { "classpath:spring/applicationContext.xml" });  
 	        TestService userService = (TestServiceImpl) ac.getBean("testService"); 
 	        Student st = new Student();
 	        //st.setId(UUID.randomUUID().toString());
